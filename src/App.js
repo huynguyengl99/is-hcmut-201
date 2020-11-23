e3c9a4a71394f5e8262c98aa1c016d08bd80884b
Nguyen Dang Huy <huy.nguyen.gl99@hcmut.edu.vn>
2020-11-23 23:37:39 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl'
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+// import logo from './logo.svg'
+import './App.css'
+
+const Map = ReactMapboxGl({
+  accessToken: process.env.REACT_APP_MAPBOX_GL_TOKEN,
+})
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: '100vh',
+          width: '100vw',
+        }}
+      >
+        <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+          <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+        </Layer>
+      </Map>
+      ;
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
